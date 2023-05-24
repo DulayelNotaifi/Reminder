360f8f3d0a576b64bd6dabff76e89a21356fd281
cady11 <113461945+cady11@users.noreply.github.com>
2023-05-24 20:58:27 +0300

    design of activity 2

---
+++ app/src/main/java/com/example/notification/Activity2.java
@@ -0,0 +1,16 @@
+package com.example.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+public class Activity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
