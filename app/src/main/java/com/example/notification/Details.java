37a5ed2ea92430dbd8adfe9c992566416abe4db1
MahaMohammadS <mahamohammadqs@gmail.com>
2023-05-29 00:54:21 +0300

    create new class, and its xml file

---
+++ app/src/main/java/com/example/notification/Details.java
@@ -0,0 +1,41 @@
+package com.example.notification;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Details extends AppCompatActivity {
+    TextView txt1, txt2, txt3, txt4, txt5, txt6;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle e = getIntent().getExtras();
+        if (e != null) {
+            String title = e.getString("title");
+            String notes = e.getString("Notes");
+            String date = e.getString("eventDate");
+            String time = e.getString("eventTime");
+            String pri = e.getString("Priority");
+            String type = e.getString("Type");
+
+
+            txt1 = (TextView) txt1.findViewById(R.id.textView99);
+            txt2 = (TextView) txt2.findViewById(R.id.textView111);
+            txt3 = (TextView) txt3.findViewById(R.id.textView400);
+            txt4 = (TextView) txt4.findViewById(R.id.textView105);
+            txt5 = (TextView) txt5.findViewById(R.id.textView166);
+            txt6 = (TextView) txt6.findViewById(R.id.textView177);
+            txt1.setText(title);
+            txt2.setText(notes);
+            txt3.setText(date);
+            txt4.setText(time);
+            txt5.setText(pri);
+            txt6.setText(type);
+
+        }
+
+    }
+}
